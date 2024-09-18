a5472c643a98a304ff03b28dcba173eb933f57b6
shishirps7 <106147525+shishirps7@users.noreply.github.com>
2024-09-18 16:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let boxes=document.querySelectorAll(".ccols");
+let num=document.querySelector("#num");
+let col=document.querySelector("#col");
+let btn=document.querySelector("button");
+btn.addEventListener("click",(e)=>{
+	e.preventDefault()
+	let x=col.value;
+	let y=parseInt(num.value)-1;
+	boxes[y].style.backgroundColor=x
+});
+
+
